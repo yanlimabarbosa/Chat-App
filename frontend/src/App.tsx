ea3f75e11300deb207d16dd49089484c6ba30491
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-27 09:20:27 -0300

    feat: Add a "No Chat Selected" placeholder
    Adds a placeholder component to the conversation area when no chat is selected. This component provides a user-friendly indication of the current state and guides the user towards selecting a chat.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import { Route, Routes } from "react-router-dom"
-import { Login } from "./pages/login"
+import { Home } from "./pages/home"
 
 export function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      <Home />
     </div>
   )
 }
