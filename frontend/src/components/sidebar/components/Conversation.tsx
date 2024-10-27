8877d2e932f0d2b283ac6d1aa125e0268cef6aa0
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-27 08:23:26 -0300

    feat: Add Conversation component
    Adds a new component that displays a conversation in the sidebar, including user avatar, name, and last message. This component will be used for displaying conversations with other users.

---
+++ frontend/src/components/sidebar/components/Conversation.tsx
@@ -0,0 +1,25 @@
+export function Conversation() {
+  return (
+    <>
+      <div className="flex gap-3 items-center hover:bg-blue-300 rounded px-2 py-1 cursor-pointer">
+        <div className="avatar online">
+          <div className="w-10 rounded-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              alt="user avatar"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">John Doe</p>
+            <span className="text-xl">🎃</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="divider my-0 py-0 h-1" />
+    </>
+  )
+}
