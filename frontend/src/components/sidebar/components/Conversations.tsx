accccf769315545f83645a7e409326aea9f3ff2d
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-27 08:23:35 -0300

    feat: Introduce Conversations component
    Adds a new Conversations component to display a list of conversations. This component will be used to show all active conversations and allows for navigation between them.

---
+++ frontend/src/components/sidebar/components/Conversations.tsx
@@ -0,0 +1,12 @@
+import { Conversation } from "./Conversation"
+
+export function Conversations() {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </div>
+  )
+}
