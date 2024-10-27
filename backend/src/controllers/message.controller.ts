aa3c6c1a3be12ba75023d6a192194930d420e77a
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-27 04:40:31 -0300

    feat: Add message retrieval and sidebar user fetching functionality
    This commit introduces endpoints for retrieving messages between users and fetching users for the sidebar. The `getMessages` endpoint retrieves messages based on the logged-in user and the user being chatted with, while `getUsersForSidebar` fetches users excluding the currently logged-in user, preparing data for the sidebar display. This functionality enhances the message exchange and user browsing capabilities of the application.

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -52,7 +52,62 @@ export const sendMessage = async (req: Request, res: Response) => {
 
     res.status(201).json(newMessage)
   } catch (error: any) {
-    console.log("Error in sendMessage", error.message)
+    console.log("Error in sendMessage controller", error.message)
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+export const getMessages = async (req: Request, res: Response) => {
+  try {
+    const { id: userToChatId } = req.params
+    const senderId = req.user.id
+
+    const conversation = await prisma.conversation.findFirst({
+      where: {
+        participantsIds: {
+          hasEvery: [senderId, userToChatId],
+        },
+      },
+      include: {
+        messages: {
+          orderBy: {
+            createdAt: "asc",
+          },
+        },
+      },
+    })
+
+    if (!conversation) {
+      return res.status(200).json([])
+    }
+
+    res.status(200).json(conversation.messages)
+  } catch (error: any) {
+    console.log("Error in getMessages controller", error.message)
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+export const getUsersForSidebar = async (req: Request, res: Response) => {
+  try {
+    const authUserId = req.user.id
+
+    const users = await prisma.user.findMany({
+      where: {
+        id: {
+          not: authUserId,
+        },
+      },
+      select: {
+        id: true,
+        fullname: true,
+        profilePic: true,
+      },
+    })
+
+    res.status(200).json(users)
+  } catch (error: any) {
+    console.log("Error in getUsersForSidebar controller", error.message)
     res.status(500).json({ error: "Internal server error" })
   }
 }
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,9 +1,15 @@
 import express from "express"
 import { protectedRoute } from "../middlewares/protectedRoute.js"
-import { sendMessage } from "../controllers/message.controller.js"
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js"
 
 const router = express.Router()
 
+router.get("/conversations", protectedRoute as any, getUsersForSidebar as any)
+router.get("/:id", protectedRoute as any, getMessages as any)
 router.post("/send/:id", protectedRoute as any, sendMessage)
 
 export { router as messageRoutes }
