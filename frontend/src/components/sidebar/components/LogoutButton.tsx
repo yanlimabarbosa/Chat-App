51cbd647057bc94073d8d1411b581cd374800bc3
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-27 08:31:14 -0300

    feat: Add LogoutButton component
    Adds a LogoutButton component to the sidebar, providing a visual indication of the logout functionality. The button utilizes the `BiLogOut` icon for visual clarity.

---
+++ frontend/src/components/sidebar/components/LogoutButton.tsx
@@ -0,0 +1,9 @@
+import { BiLogOut } from "react-icons/bi"
+
+export function LogoutButton() {
+  return (
+    <div className="mt-auto">
+      <BiLogOut className="w-6 h-6 text-white cursor-pointer transition duration-100 hover:text-gray-300" />
+    </div>
+  )
+}
