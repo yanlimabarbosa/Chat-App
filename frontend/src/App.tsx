7897347658477003d784d8f6a498f377c693ecfb
Yan Lima Barbosa <yanbr763@gmail.com>
2024-11-09 11:44:22 -0300

    feat: Implement authentication and routing
    Introduce basic authentication and routing to enforce access to different pages based on user authentication status. This allows unauthorized users to access the signup and login pages while authenticated users are redirected to the home page.
    
    This commit also includes the necessary routes for each page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+import { useAuthContext } from './context/AuthContext'
 import { Home } from './pages/home'
+import { Login } from './pages/login'
+import { SignUp } from './pages/signup'
 
 export function App() {
+  const { authUser, isLoading } = useAuthContext()
+  if (isLoading) return null
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={'/login'} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUp /> : <Navigate to={'/'} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to={'/'} />}
+        />
+      </Routes>
     </div>
   )
 }
